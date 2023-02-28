143a210b072d70e134fdeef066e405dcbcdceb1b
windwingwalker <windwingwalker@icloud.com>
2023-02-28 11:17:04 -0500

    Modify para

--- src/upload-moze/index.ts
+++ src/upload-moze/index.ts
@@ -7,7 +7,7 @@ const uploadMoze = async (tgWrapper: TgWrapper, fileId: string, fileName: string
 
     const file: any = await tgWrapper.downloadFile(fileId);
     const bucketName: string = await getParameterFromSSM("/finance/s3-bucket-name");
-    await putObjectToS3(bucketName, "moze/MOZE.csv", file);
+    await putObjectToS3(bucketName, "finance/MOZE.csv", file);
 
     await tgWrapper.sendMessage('Successfully uploaded MOZE.csv');  
     
